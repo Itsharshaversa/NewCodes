8d0e52a32e6c3ea44df172e3e2a1703dc972ebf5
hArs-hit-srivastava <harshit.2327cseml102@kiet.edu>
2025-12-19 20:59:24 +0530

    fivth

---
+++ readme.md
@@ -0,0 +1,6 @@
+git status sirf unko dekhta jo cimmited ni  jo commit hogai unka ussekoi mtlb ni h ua sirf unka btaega jo comiit hone ke baad chang e ki gai h 
+
+
+
+git log btat saare commit historygit 
+git lig btyega sab commits ki histroy git log --one line  git 
---
+++ rec1.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int sum(int n ){
+    int sums =0;
+    if(n>0){
+    
+    sums = n + sum(n-1);
+    }
+    return sums;
+   
+}
+ 
+
+
+
+
+
+
+int main() {
+    int n ;
+    cin >>n;
+    // numbers(n);
+    cout<<sum(n)<<endl;
+    int ans;
+    ans = (n*(n+1))/2;
+    cout<<ans<<endl;
+
+
+    return 0;
+}
+\
