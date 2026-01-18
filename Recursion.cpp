4d08bd6e1d079b86952a773bc5da3fbe48cdceb3
hArs-hit-srivastava <harshit.2327cseml102@kiet.edu>
2026-01-18 23:45:31 +0530

    heyy

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,21 +1,35 @@
 // Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
+// #include<iostream>
 using namespace std;
-int n =0;
+int n;
 void fun(int n ){
-    if(n==5) return;
+    // if (n==0) return;
     // while (n > 0){
     // cout<<n<<endl;
     // n--; }
     // fun(n);
-    cout<<"Harshit"<<endl;
-    n++;
-    fun(n);
+    
+    // n to 1;
+    // cout<<n<<endl;
+    // n--;
+    // fun(n);
     // fun(n-1);
-    // return;
+
+    // cout<<n<<endl;
+    // n++;
+    
+    // fun(n);
+    
+    
+    if (n==0) return;
+    cout<<n<<endl;
+    fun(n-1);
+    
 }
 
 int main() {
+  cin>>n;
    fun(n);
 
     return 0;
